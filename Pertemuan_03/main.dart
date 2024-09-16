7c59e7994736b710096d817c410fdfe97d51ad04
emirabiyyu <emirabiyyu63@gmail.com>
2024-09-16 19:17:59 +0700

    change

--- Pertemuan_03/main.dart
+++ Pertemuan_03/main.dart
@@ -1,12 +1,24 @@
 void main() {
-  String test = 'test2';
-  if (test == "test1") {
-    print("Test1");
-  } else if (test == "test2") {
-    print("Test2");
-  } else {
-    print("Something else");
-  }
+  // String test = 'test2';
+  // if (test == "test1") {
+  //   print("Test1");
+  // } else if (test == "test2") {
+  //   print("Test2");
+  // } else {
+  //   print("Something else");
+  // }
 
-  if (test == "test2") print("Test2 again");
+  // if (test == "test2") print("Test2 again");
+
+  // String test = "true";
+  // if (test == "true") {
+  //   print("Kebenaran");
+  // }
+
+  // test = "true";
+  // if (test == "true") {
+  //   print("Kebenaran");
+  // } else {
+  //   print("Kesalahan");
+  // }
 }
---
+++ Pertemuan_03/readme.md
@@ -0,0 +1,6 @@
+Emir Abiyyu
+3F/2241720197
+<br>
+
+1. <Assets/Screenshot 2024-09-16 at 18.33.53.png>
+2. 
